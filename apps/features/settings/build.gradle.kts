64bfd4db9a754930cb301891bcf16c4b8610c141
Santosh Jangid <271889561+im-tnyx@users.noreply.github.com>
2026-07-18 09:58:21 +0530

    build(settings): add activity result compose APIs

--- apps/features/settings/build.gradle.kts
+++ apps/features/settings/build.gradle.kts
@@ -34,6 +34,7 @@ dependencies {
     implementation(project(":core"))
 
     implementation(platform(libs.androidx.compose.bom))
+    implementation(libs.androidx.activity.compose)
     implementation(libs.androidx.compose.material3)
     implementation(libs.androidx.compose.material.icons.extended)
     implementation(libs.androidx.compose.ui)
